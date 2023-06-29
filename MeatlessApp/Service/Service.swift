8dadc881f709b19b131ab42cb026b96d56b1e18a
Augusto Baroni Simionato <72254418+AugustoBSimionato@users.noreply.github.com>
2023-06-29 09:49:24 -0300

    first commit

--- MeatlessApp/Service/Service.swift
+++ MeatlessApp/Service/Service.swift
@@ -3,8 +3,29 @@ import Foundation
 struct Service {
     
     func fetchRestaurantList(completion: @escaping ([Restaurant]?)->Void) {
-        
         // STORY 1: Implement the API integration to fetch restaurant list.
-         
+        guard let url = URL(string: "https://raw.githubusercontent.com/devpass-tech/meatless-api/main/restaurant_list.json") else { return }
+        
+        let session = URLSession.shared
+        var request = URLRequest(url: url)
+        
+        request.httpMethod = "GET"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let task = session.dataTask(with: request) { (data, response, error) in
+            if let error = error { return }
+
+            guard let data = data else { return }
+
+            do {
+                let decoder = JSONDecoder()
+                let restaurants = try decoder.decode([Restaurant].self, from: data)
+                
+            } catch {
+                
+            }
+        }
+
+        task.resume()
     }
 }
